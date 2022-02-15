84f16e473e7722df347c6273da41848eafedd325
webster <wbian618@gmail.com>
2022-02-15 18:24:28 -0500

    CSS background

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Navbar/>
       </header>
+        <div class="space stars1"></div>
+        <div class="space stars2"></div>
+        <div class="space stars3"></div>
     </div>
   );
 }
---
+++ portfolio/src/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function Navbar() {
+    return (
+        <div id='navbar'>
+            <h3 className='navbar-text'>Home</h3> 
+            <h3 className='navbar-text'>About</h3>
+            <h3 className='navbar-text'>Projects</h3>
+            <h3 className='navbar-text'>Resume</h3>
+
+        </div>
+    )
+    
+}
+
+export default Navbar;
