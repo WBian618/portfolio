0922701e990ce19492d33a1695d51880d4f9d637
webster <wbian618@gmail.com>
2022-02-16 11:48:19 -0500

    added astro image and proper navbar alignment

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Navbar from './Navbar';
+import astronaut from './Assets/astronaut.png'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <div class="space stars1"></div>
         <div class="space stars2"></div>
         <div class="space stars3"></div>
+        <img id='astronaut' src={astronaut} alt={astronaut}/>
     </div>
   );
 }
--- portfolio/src/Navbar.js
+++ portfolio/src/Navbar.js
@@ -4,11 +4,10 @@ import './App.css';
 function Navbar() {
     return (
         <div id='navbar'>
-            <h3 className='navbar-text'>Home</h3> 
-            <h3 className='navbar-text'>About</h3>
-            <h3 className='navbar-text'>Projects</h3>
-            <h3 className='navbar-text'>Resume</h3>
-
+                <h3 className='navbar-text'>Home</h3> 
+                <h3 className='navbar-text'>About</h3>
+                <h3 className='navbar-text'>Projects</h3>
+                <h3 className='navbar-text'>Resume</h3>
         </div>
     )
     
